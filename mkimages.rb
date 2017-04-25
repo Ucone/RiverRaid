900506c1f72b5ef38ca7a1b7014232566033f77b
256279 <iaroslav.gridin@student.tut.fi>
2017-04-25 13:59:13 +0300

    Add images' rendering script

---
+++ mkimages.rb
@@ -0,0 +1,12 @@
+require 'fileutils'
+
+Dir.chdir("assets/images")
+Dir.glob("**/*.xcf").each do |f|
+	fname = "../../code/River_Raid/images/#{f.gsub(/xcf$/,"png")}"
+	if !File.exists? fname or File.mtime(f) > File.mtime(fname)
+		FileUtils.mkpath(fname.gsub(%r|/[^/]+$|,""))
+		cmd = "xcf2png #{f} > #{fname}"
+		puts cmd
+		`#{cmd}`
+	end
+end
